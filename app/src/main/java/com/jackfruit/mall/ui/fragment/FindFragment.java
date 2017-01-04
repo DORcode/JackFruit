081da44674d7551ed714ac896c3a7e9afd82d4eb
pc <dataanalyst@163.com>
2017-01-04 13:30:40 +0800

    友盟分享

--- app/src/main/java/com/jackfruit/mall/ui/fragment/FindFragment.java
+++ app/src/main/java/com/jackfruit/mall/ui/fragment/FindFragment.java
@@ -1,18 +1,39 @@
 package com.jackfruit.mall.ui.fragment;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.jackfruit.mall.R;
+import com.jackfruit.mall.ui.activity.LocationActivity;
+import com.jackfruit.mall.umeng_lib.CheckActivity;
+import com.jackfruit.mall.umeng_lib.ShareMenuActivity;
+import com.jackfruit.mall.umeng_lib.model.Defaultcontent;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.shareboard.ShareBoardConfig;
+import com.umeng.socialize.shareboard.SnsPlatform;
+import com.umeng.socialize.utils.ShareBoardlistener;
 
-public class FindFragment extends Fragment {
+import java.lang.ref.WeakReference;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class FindFragment extends BaseFragment {
     private static final String ARG_PARAM1 = "param1";
+    private View view;
 
+    private ShareAction mShareAction;
+    private UMShareListener mShareListener;
     private String mParam1;
 
 
@@ -39,7 +60,117 @@ public class FindFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_find, container, false);
+        view = inflater.inflate(R.layout.fragment_find, container, false);
+        ButterKnife.bind(this, view);
+        mShareListener = new CustomShareListener(getActivity());
+        /*增加自定义按钮的分享面板*/
+        mShareAction = new ShareAction(getActivity()).setDisplayList(
+                SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE, SHARE_MEDIA.WEIXIN_FAVORITE,
+                SHARE_MEDIA.SINA, SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE,
+                SHARE_MEDIA.ALIPAY, SHARE_MEDIA.RENREN, SHARE_MEDIA.DOUBAN,
+                SHARE_MEDIA.SMS, SHARE_MEDIA.EMAIL, SHARE_MEDIA.YNOTE,
+                SHARE_MEDIA.EVERNOTE, SHARE_MEDIA.LAIWANG, SHARE_MEDIA.LAIWANG_DYNAMIC,
+                SHARE_MEDIA.LINKEDIN, SHARE_MEDIA.YIXIN, SHARE_MEDIA.YIXIN_CIRCLE,
+                SHARE_MEDIA.TENCENT, SHARE_MEDIA.FACEBOOK, SHARE_MEDIA.TWITTER,
+                SHARE_MEDIA.WHATSAPP, SHARE_MEDIA.GOOGLEPLUS, SHARE_MEDIA.LINE,
+                SHARE_MEDIA.INSTAGRAM, SHARE_MEDIA.KAKAO, SHARE_MEDIA.PINTEREST,
+                SHARE_MEDIA.POCKET, SHARE_MEDIA.TUMBLR, SHARE_MEDIA.FLICKR,
+                SHARE_MEDIA.FOURSQUARE, SHARE_MEDIA.MORE)
+                .addButton("umeng_sharebutton_copy", "umeng_sharebutton_copy", "umeng_socialize_copy", "umeng_socialize_copy")
+                .addButton("umeng_sharebutton_copyurl", "umeng_sharebutton_copyurl", "umeng_socialize_copyurl", "umeng_socialize_copyurl")
+                .setShareboardclickCallback(new ShareBoardlistener() {
+                    @Override
+                    public void onclick(SnsPlatform snsPlatform, SHARE_MEDIA share_media) {
+                        if (snsPlatform.mShowWord.equals("umeng_sharebutton_copy")) {
+                            Toast.makeText(getActivity(), "复制文本按钮", Toast.LENGTH_LONG).show();
+                        } else if (snsPlatform.mShowWord.equals("umeng_sharebutton_copyurl")) {
+                            Toast.makeText(getActivity(), "复制链接按钮", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            new ShareAction(getActivity()).withText(Defaultcontent.text + "来自友盟自定义分享面板")
+                                    .setPlatform(share_media)
+                                    .setCallback(mShareListener)
+                                    .share();
+                        }
+                    }
+                });
+
+        return view;
     }
 
+    private static class CustomShareListener implements UMShareListener {
+
+        private WeakReference<ShareMenuActivity> mActivity;
+
+        private CustomShareListener(Activity activity) {
+            mActivity = new WeakReference(activity);
+        }
+
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+
+            if (platform.name().equals("WEIXIN_FAVORITE")) {
+                Toast.makeText(mActivity.get(), platform + " 收藏成功啦", Toast.LENGTH_SHORT).show();
+            } else {
+                if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
+                        && platform != SHARE_MEDIA.EMAIL
+                        && platform != SHARE_MEDIA.FLICKR
+                        && platform != SHARE_MEDIA.FOURSQUARE
+                        && platform != SHARE_MEDIA.TUMBLR
+                        && platform != SHARE_MEDIA.POCKET
+                        && platform != SHARE_MEDIA.PINTEREST
+                        && platform != SHARE_MEDIA.LINKEDIN
+                        && platform != SHARE_MEDIA.INSTAGRAM
+                        && platform != SHARE_MEDIA.GOOGLEPLUS
+                        && platform != SHARE_MEDIA.YNOTE
+                        && platform != SHARE_MEDIA.EVERNOTE) {
+                    Toast.makeText(mActivity.get(), platform + " 分享成功啦", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            if (platform != SHARE_MEDIA.MORE && platform != SHARE_MEDIA.SMS
+                    && platform != SHARE_MEDIA.EMAIL
+                    && platform != SHARE_MEDIA.FLICKR
+                    && platform != SHARE_MEDIA.FOURSQUARE
+                    && platform != SHARE_MEDIA.TUMBLR
+                    && platform != SHARE_MEDIA.POCKET
+                    && platform != SHARE_MEDIA.PINTEREST
+                    && platform != SHARE_MEDIA.LINKEDIN
+                    && platform != SHARE_MEDIA.INSTAGRAM
+                    && platform != SHARE_MEDIA.GOOGLEPLUS
+                    && platform != SHARE_MEDIA.YNOTE
+                    && platform != SHARE_MEDIA.EVERNOTE) {
+                Toast.makeText(mActivity.get(), platform + " 分享失败啦", Toast.LENGTH_SHORT).show();
+                if (t != null) {
+                    com.umeng.socialize.utils.Log.d("throw", "throw:" + t.getMessage());
+                }
+            }
+
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+
+            Toast.makeText(mActivity.get(), platform + " 分享取消了", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick({R.id.tv_find, R.id.tv_appkey_check})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_find:
+                ShareBoardConfig config = new ShareBoardConfig();
+                config.setMenuItemBackgroundShape(ShareBoardConfig.BG_SHAPE_NONE);
+                mShareAction.open(config);
+                break;
+            case R.id.tv_appkey_check:
+                goToActivity(CheckActivity.class);
+                break;
+        }
+
+    }
 }
---
+++ umeng_lib/src/main/java/com/jackfruit/mall/umeng_lib/yxapi/YXEntryActivity.java
@@ -0,0 +1,9 @@
+
+package com.jackfruit.mall.umeng_lib.yxapi;
+
+
+import com.umeng.socialize.activity.YXCallbackActivity;
+
+public class YXEntryActivity extends YXCallbackActivity {
+
+}
