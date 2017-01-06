50feef76e30edf4c586dad1836e18e9960028831
pc <dataanalyst@163.com>
2017-01-06 13:35:35 +0800

    删除友盟不需要jar包 修改安装名称显示错误，加一个values-zh

--- umeng_lib/src/main/java/com/jackfruit/mall/umeng_lib/wxapi/WXEntryActivity.java
+++ umeng_lib/src/main/java/com/jackfruit/mall/umeng_lib/wxapi/WXEntryActivity.java
@@ -1,9 +1,10 @@
 package com.jackfruit.mall.umeng_lib.wxapi;
 
 
-import com.umeng.weixin.callback.WXCallbackActivity;
 
 //import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
 
--- umeng_lib/src/main/java/com/jackfruit/mall/umeng_lib/yxapi/YXEntryActivity.java
+++
@@ -1,9 +0,0 @@
-
-package com.jackfruit.mall.umeng_lib.yxapi;
-
-
-import com.umeng.socialize.activity.YXCallbackActivity;
-
-public class YXEntryActivity extends YXCallbackActivity {
-
-}
